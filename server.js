06c3110e1ce991c2c4c4e8949039292f46aafab0
Ryan Siverson <rcsskier@mac.com>
2020-05-19 10:05:23 -0600

    Linking MongoDb

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,10 +16,12 @@ app.use(require("./routes/view.js"));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
